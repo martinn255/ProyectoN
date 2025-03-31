24b93b1bb5cef5b3f909b1a4eb28115cceea3874
martinn255 <espinosamartin58@gmail.com>
2025-03-31 12:22:05 -0300

    resolver del espaciado

--- src/Cadena.java
+++ src/Cadena.java
@@ -7,14 +7,14 @@ public class Cadena{
     }
     int cantidadPalabras(){
         int cont = 0, i;
-        char c;
+        char c, prev= ' ';
         for(i=0 ; i < pala.length() ; i++ ){
             c = pala.charAt(i);             //Pone en una variable char un caracter de las palabras
             if(Character.isWhitespace(c)){  //Pregunta por el espaciado en una frase 
                 cont++;
-                /*if(Character.isWhitespace(pala.charAt(i))){   //Si hay mas de un espacio no lo cuenta
+                if(Character.isWhitespace(pala.charAt(i+1))){   //Si hay mas de un espacio no lo cuenta
                     cont--;
-                }*/
+                }
             }
         }
         return cont+1;
