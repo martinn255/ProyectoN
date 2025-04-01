71da46732d80221d9ddeb0c8b51df05b79e09df8
martinn255 <espinosamartin58@gmail.com>
2025-04-01 00:20:00 -0300

    complementarios

--- src/Cadena.java
+++ src/Cadena.java
@@ -7,7 +7,7 @@ public class Cadena{
     }
     int cantidadPalabras(){
         int cont = 0, i;
-        char c, prev= ' ';
+        char c;
         for(i=0 ; i < pala.length() ; i++ ){
             c = pala.charAt(i);             //Pone en una variable char un caracter de las palabras
             if(Character.isWhitespace(c)){  //Pregunta por el espaciado en una frase 
@@ -18,5 +18,37 @@ public class Cadena{
             }
         }
         return cont+1;
+    }
+    int cantidadVocales(){
+        int contVoc = 0 ,i;
+        char c;
+        for(i=0 ; i < pala.length() ; i++ ){
+            c = pala.charAt(i);
+            if(Character.isLetter(c)){//verifica que sea una letra 
+                if(c != Character.toLowerCase(c)){
+                    c= Character.toLowerCase(c); //Convierte a minuscula el caracter
+                }
+                if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                    contVoc++;
+                }
+            }
+        }
+        return contVoc;
+    }
+    int cantidadConsonantes(){
+        int contCons = 0, i;
+        char c;
+        for(i=0; i< pala.length() ; i++){
+            c = pala.charAt(i);
+            if(Character.isLetter(c)){
+                if(c!=Character.toLowerCase(c)){
+                    c = Character.toLowerCase(c);
+                }
+                if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u'){
+                    contCons++;
+                }
+            }
+        }
+        return contCons;
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,18 @@
 public class Principal {
     public static void main(String[] args) {
         Cadena n = new Cadena();
-        int cantC, cantP;
+        int cantC, cantP, cantVo, cantConso;
 
         n.pala ="hola chicos de POO 2025" ;
         cantC = n.cantidadCaracteres();
         cantP = n.cantidadPalabras();
+        cantVo = n.cantidadVocales();
+        cantConso = n.cantidadConsonantes();
 
-        System.out.println(n.pala);
-        System.out.println(cantC);
-        System.out.println(cantP);
-    }
+        System.out.println("Frase utilizada: " + n.pala);
+        System.out.println("Cantidad de Caracteres: " + cantC);
+        System.out.println("Cantidad de Palabras: " + cantP);
+        System.out.println("Cantidad de Vocales: " + cantVo);
+        System.out.println("Cantidad de Consonates: " + cantConso);
+    }   
 }
